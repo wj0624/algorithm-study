8b2aca65ed739930d28b920345787c0d5d873596
wonjeong lim <wonjeong4538@gmail.com>
2023-03-23 22:04:59 +0900

    0323:[25305, O]

--- sort/25305.py
+++ sort/25305.py
@@ -1,8 +1,6 @@
 # map(f, iterable) : 입력 받은 데이터의 각 요소에 함수 f를 적용한 결과를 리턴
 #.split() : 문자열 나누기, () 공백 기준
 n, k = map(int, input().split()) # n:학생 수, k: 수상자 수
-
 num = list(map(int, input().split()))
 num.sort(reverse=True) #내림차순 정렬
-
 print(num[k-1])
