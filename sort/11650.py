8d543f3b91b8314b6bfd9828b60b00ed9584c874
wonjeong lim <wonjeong4538@gmail.com>
2023-03-26 02:18:06 +0900

    0326:[11650, O]

--- sort/11650.py
+++ sort/11650.py
@@ -5,6 +5,9 @@ arr = []
 for i in range(n):
     x, y = map(int, sys.stdin.readline().split())
     arr.append((x,y))
-arr.sort()
+
+#sort 옵션 : (key = lambda x(x[0],x[1])) -> 첫번째값 기준 정렬, 값 같으면 두번째값 기준
+arr.sort(key = lambda x:(x[0], x[1]))
+
 for i in range(len(arr)):
     print(arr[i][0],arr[i][1])
