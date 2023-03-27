eaea31422c7338ab1acea50cf53a5720a3c3652f
wonjeong lim <wonjeong4538@gmail.com>
2023-03-28 02:42:13 +0900

    0328:[1181, O]

--- sort/1181.py
+++ sort/1181.py
@@ -3,7 +3,7 @@ n = int(sys.stdin.readline())
 arr = []
 for i in range(n):
     arr.append(sys.stdin.readline().strip())
-arr=list(set(arr)) #중복 제거
-arr.sort(key=lambda x:(len(x),x)) #정렬 with length
+arr = list(set(arr)) #중복 제거
+arr.sort(key=lambda x: (len(x), x)) #정렬 with length
 for i in range(len(arr)):
     print(arr[i])
