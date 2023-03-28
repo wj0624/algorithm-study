9b2f464041129039bd5c8ee2089a8ad1e7bef43b
wonjeong lim <wonjeong4538@gmail.com>
2023-03-28 23:18:14 +0900

    0328:[10815, O]

--- sort/10815.py
+++ sort/10815.py
@@ -1,8 +1,11 @@
 #시간초과
+#card list를 set으로 변경 -> 두 숫자 카드에 같은 수 적혀있는 경우 X
+#리스트에서의 x in s 연산의 평균 시간 복잡도 : O(n)
+#세트에서의 x in s 연산의 평균 시간 복잡도 : O(1)
 import sys
 
 n = int(sys.stdin.readline())
-card = list(map(int, sys.stdin.readline().split()))
+card = set(map(int, sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
 num = list(map(int, sys.stdin.readline().split()))
 
