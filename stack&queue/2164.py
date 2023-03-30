64594064c34e0cc01b4d3d278c6a026002b86b37
wonjeong lim <wonjeong4538@gmail.com>
2023-03-30 15:46:23 +0900

    0330:[2164, X]

--- stack&queue/2164.py
+++ stack&queue/2164.py
@@ -4,6 +4,5 @@ card = [x for x in range(1, n+1)]
 #0번째를 제일 위
 for i in range(n-1):
     card.pop(0)
-    card.insert(len(card), card[0])
-    card.pop(0)
+    card.append(card.pop(0))
 print(card[0])
