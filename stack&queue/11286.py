d3bd68938d00ccd322253240c98ed096d1dc4469
wonjeong lim <wonjeong4538@gmail.com>
2023-04-05 01:47:55 +0900

    0404:[11286, O]

---
+++ stack&queue/11286.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+n = int(input())
+heap = []
+
+for i in range(n):
+    x = int(sys.stdin.readline())
+    if x:
+        heapq.heappush(heap,(abs(x),x))
+    else:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
