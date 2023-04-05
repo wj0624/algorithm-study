d50ec748fcad7bbc208929b27d874c647210059e
wonjeong lim <wonjeong4538@gmail.com>
2023-04-05 21:14:18 +0900

    0403:[1874, O]

--- stack&queue/1874.py
+++ stack&queue/1874.py
@@ -1,22 +1,20 @@
 import sys
 n = int(input())
 cnt = 1
-flag = 0
 stack = []
 ans = []
 for i in range(n):
     num = int(sys.stdin.readline())
-    while cnt<=num:
+    while cnt<=num: #push
         stack.append(cnt)
         ans.append('+')
+        #print('+') 바로 프린트하게 되면 수열을 만들 수 없는데도, 그 전까지 수행한 결과를 출력하게 됨
         cnt+=1
-    if stack.pop()==num:
+    if stack.pop()==num: #pop
         ans.append('-')
+        #print('-')
     else:
-        flag=1
+        print("NO")
         break
-if flag:
-    print("NO")
 else:
-    for i in ans:
-        print(i)
+    print("\n".join(ans))
