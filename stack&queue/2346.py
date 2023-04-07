72d1349374dcaf0f956f24436d73feba0d285863
wonjeong lim <wonjeong4538@gmail.com>
2023-04-07 21:50:14 +0900

    0403:[2346, O]

--- stack&queue/2346.py
+++ stack&queue/2346.py
@@ -4,11 +4,12 @@ n= int(input())
 #enumerate 사용 시 index와 원소 tuple 형태로 반환
 dq = deque(enumerate(map(int, input().split())))
 ans = []
+print(dq)
 while dq:
-    index, paper = dq.popleft()
-    ans.append(index+1)
+    index, paper = dq.popleft() #순서대로 풍선의 번호와 종이에 적힌 값을 반환
+    ans.append(index+1) #index가 0부터 시작하므로 +1을 해서 추가해준다
     if paper > 0:
-        dq.rotate(-(paper-1))
+        dq.rotate(-(paper-1)) #이미 풍선이 popleft를 했기 때문에 왼쪽으로 한칸씩 회전되어 있으므로 paper-1만큼 회전
     elif paper < 0:
         dq.rotate(-paper)
 for i in ans:
