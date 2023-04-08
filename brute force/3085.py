1ad44023376fe5d292f996344d86c34d5fcaf3eb
wonjeong lim <wonjeong4538@gmail.com>
2023-04-09 02:16:04 +0900

    0408:[2503, O]

---
+++ brute force/3085.py
@@ -0,0 +1,39 @@
+import sys
+n = int(input())
+candy = [list(sys.stdin.readline().strip()) for _ in range(n)]
+ans = 0
+
+def cnt(candy):
+    Max = 1
+    for i in range(n):
+        #행 탐색
+        cnt = 1
+        for j in range(1,n):
+            if candy[i][j] == candy[i][j-1]:
+                cnt += 1
+            else:
+                cnt = 1
+            Max = max(Max, cnt)
+        #열 탐색
+        cnt = 1
+        for j in range(1,n):
+            if candy[j][i] == candy[j-1][i]:
+                cnt += 1
+            else :
+                cnt = 1
+            Max = max(Max, cnt)
+    return Max
+
+for i in range(n):
+    for j in range(n):
+        #행 바꾸기
+        if i+1 < n:
+            candy[i][j], candy[i+1][j] = candy[i+1][j], candy[i][j] #swap
+            ans = max(ans, cnt(candy))
+            candy[i][j], candy[i+1][j] = candy[i+1][j], candy[i][j] #원래대로
+        #열 바꾸기
+        if j+1 < n:
+            candy[i][j], candy[i][j+1] = candy[i][j+1], candy[i][j] #swap
+            ans = max(ans, cnt(candy))
+            candy[i][j], candy[i][j + 1] = candy[i][j+1], candy[i][j] #swap
+print(ans)
