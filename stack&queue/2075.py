79a0182006c46ffbb2232b0d52e210acb5298c5a
wonjeong lim <wonjeong4538@gmail.com>
2023-04-11 22:31:56 +0900

    0404:[2075, O]

--- stack&queue/2075.py
+++ stack&queue/2075.py
@@ -5,12 +5,13 @@ n = int(input())
 heap = []
 for i in range(n):
     num = list(map(int, sys.stdin.readline().split()))
-    if not heap:
+    if not heap: #첫번째 입력
         for j in num:
             heapq.heappush(heap, j)
+            print(heap)
     else:
-        for j in num:
-            if heap[0] < j:
-                heapq.heappush(heap, j)
-                heapq.heappop(heap)
+        for j in num: #항상 길이 n만큼 유지
+            if heap[0] < j: #최소값 보다 클 때
+                heapq.heappush(heap, j) #push
+                heapq.heappop(heap) #최소값은 pop
 print(heap[0])
