cf626cbaee75185b0d7cdfca0178d29685997914
wonjeong lim <wonjeong@gmail.com>
2023-08-15 18:00:45 +0900

    23.08.15, P

--- goormton/src/day2.java
+++ goormton/src/day2.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+//프로젝트 매니징
 class day2{
 	static int N, T, M, time;
 	static int ans_h, ans_m;
@@ -18,6 +19,7 @@ class day2{
 		
 		for(int i=0; i<N; i++) {
 			time = Integer.parseInt(br.readLine());
+			//입력된 시간(분단위) 더해주기
 			if(time+ans_m<60) {
 				ans_m += time;
 			}
@@ -25,6 +27,8 @@ class day2{
 				ans_h += time/60;
 				ans_m += time%60;
 			}
+			
+			//더하고 난 후 시간 다시 한 번 계산
 			if(ans_m>=60) {
 				ans_h += ans_m/60;
 				ans_m = ans_m%60;
