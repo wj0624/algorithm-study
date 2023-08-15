321d57763875a633c3de97e85d0916b750774a1e
wonjeong lim <wonjeong@gmail.com>
2023-08-15 17:58:42 +0900

    23.08.14

--- goormton/src/day1.java
+++ goormton/src/day1.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+//운동 중독 플레이어
 public class day1 {
 	static double W, R;
 	static int ans;
@@ -9,6 +10,9 @@ public class day1 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		W = Double.parseDouble(st.nextToken());
 		R = Double.parseDouble(st.nextToken());
+		
+		//double끼리 연산할 때는 숫자도 소숫점으로 나타내어야 함
+		//int로 casting하면 소숫점이하 버림
 		ans = (int) ((1+R/30.0)*W);
 		
 		System.out.println(ans);
