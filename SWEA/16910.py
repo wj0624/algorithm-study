47c21badfe2c3d070f6754e3a2e2c899f309fc0d
wonjeong lim <wonjeong@gmail.com>
2023-11-10 13:18:44 +0900

    .

---
+++ SWEA/16910.py
@@ -0,0 +1,9 @@
+T = int(input())
+for test_case in range(1, T+1):
+    N = int(input())
+    cnt=0
+    for x in range(1, N+1) :
+        for y in range(N+1):
+            if (x*x+y*y) <= (N*N) :
+                cnt += 1
+    print(f"#{test_case} {cnt*4+1}")
---
+++ SWEA/17319.py
@@ -0,0 +1,15 @@
+T = int(input())
+for test_case in range(1, T+1):
+    N = int(input())
+    str = list(input())
+    ans = "No"
+    mid = int(N/2)
+    if N%2 == 0:
+        for i in range(mid) :
+            if(str[i]==str[i+mid]):
+                ans = "Yes"
+            else :
+                ans = "No"
+                break
+
+    print(f"#{test_case} {ans}")
