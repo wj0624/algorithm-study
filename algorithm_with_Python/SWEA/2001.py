0ddc1cbe37f8124d6bcdf3dcbdd73c2c08ef8e1d
wonjeong lim <wonjeong@gmail.com>
2023-11-17 01:13:27 +0900

    23.11.16

---
+++ algorithm_with_Python/SWEA/2001.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+for test_case in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    ans = 0
+    for i in range(N-M+1):
+        for j in range(N-M+1):
+            sum = 0
+            for k in range(M):
+                for l in range(M):
+                    sum += arr[i+k][j+l]
+            ans = max(ans, sum)
+
+    print(f"#{test_case} {ans}")
