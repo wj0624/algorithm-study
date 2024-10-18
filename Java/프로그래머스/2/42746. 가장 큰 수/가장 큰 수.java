7424dd1209d38e182bf951546e4c56a91cd9c2cf
Wonjeong Lim <128574107+wj0624@users.noreply.github.com>
2024-10-19 01:09:02 +0900

    [level 2] Title: 가장 큰 수, Time: 248.39 ms, Memory: 130 MB -BaekjoonHub

--- Java/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ Java/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,28 +1,32 @@
 import java.io.*;
 import java.util.*;
 class Solution {
+    static boolean[] visited;
+    static int depth, answer;
+    static ArrayList<String> list = new ArrayList<>();
     public String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
-        for(int i=0; i<numbers.length; i++){
-            arr[i] = String.valueOf(numbers[i]);
+        depth = numbers.length;
+        visited = new boolean[depth];
+        for(int num : numbers){
+            list.add(String.valueOf(num));
         }
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-
-        return arr[0].equals("0") ? "0" : String.join("", arr);
+        Collections.sort(list, Collections.reverseOrder());
+        combi("", String.join("", list), 0);
+        return String.valueOf(answer);
     }
-//     static void combi(int idx, String cur, int cnt){
-//         if(cnt == depth){
-//             // System.out.println(cur);
-//             answer = Math.max(answer, Integer.parseInt(cur));
-//         }
+    static void combi(String cur, String other, int cnt){
+        if(cnt == depth){
+            System.out.println(cur);
+            answer = Math.max(answer, Integer.parseInt(cur));
+        }
         
-//         for(int i=0; i<depth; i++){
-//             String s = list.get(i);
-//             if(!visited[i]){
-//                 visited[i] = true;
-//                 combi(idx+1, cur+s, cnt+1);
-//                 visited[i] = false;
-//             } 
-//         }
-//     }
+        for(int i=0; i<depth; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                combi(cur+list.get(i), other.substring(i+1), cnt+1);
+                visited[i] = false;
+            }
+            
+        }
+    }
 }
